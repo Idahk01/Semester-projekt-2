5dd8396df7d0cc6fd9707f7ff13f8909d67233e2
JeppeVit <132815647+JeppeVit@users.noreply.github.com>
2025-05-09 12:54:32 +0200

    Update HomeAssistant.cs

--- TiltBuddy/HomeAssistant.cs
+++ TiltBuddy/HomeAssistant.cs
@@ -6,27 +6,36 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-public class HomeAssistantController
+class HomeAssistant
 {
     // URL til din Home Assistant instans
     private const string HaBaseUrl = "http://172.20.10.13:8123";
     // Din Long-Lived Access Token til Home Assistant
     private const string HaToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmMDFjOGJjMzBhNGE0NDAzYWJiNWU5NjdjYzdiZjU1MiIsImlhdCI6MTc0Mzk2OTY5NiwiZXhwIjoyMDU5MzI5Njk2fQ.perXXUfe-44-SzCOS1t1h-4BZ0AxvOiRTbI-16yxVvU";
     // ID på den standardenhed (lampe) der skal styres i Home Assistant
-    private const string DefaultEntityId = "input_boolean.fake_lamp";
+    
+    private const string KontaktId = "switch.innr_sp_220";
+    
+    //private const string LampeId = "switch.innr_sp_220";
+    // Fake Lamp: private const string DefaultEntityId = "input_boolean.fake_lamp";
 
     private readonly HttpClient _httpClient;
     
-    public HomeAssistantController()
+    public HomeAssistant()
     {
         _httpClient = new HttpClient();
         _httpClient.DefaultRequestHeaders.Authorization =
             new AuthenticationHeaderValue("Bearer", HaToken);
     }
     
-    public async Task<bool> ToggleDefaultEntityAsync()
+    /*public async Task<bool> ToggleLamp()
     {
-        return await ToggleEntityAsync(DefaultEntityId);
+        return await ToggleEntityAsync(LampeId);
+    }*/
+    
+    public async Task<bool> ToggleTV()
+    {
+        return await ToggleEntityAsync(KontaktId);
     }
     
     public async Task<bool> ToggleEntityAsync(string entityId)

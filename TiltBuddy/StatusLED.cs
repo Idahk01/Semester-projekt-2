c5b6fdb87314fc95561deb7464ef54b4c4a01d8d
frejarandel <freja.randel@gmail.com>
2025-05-13 08:42:10 +0200

    Update StatusLED.cs

--- TiltBuddy/StatusLED.cs
+++ TiltBuddy/StatusLED.cs
@@ -1,1 +1,28 @@
+using System.Device.Pwm;
+using System.Device.Pwm.Drivers;
+
+public class StatusLED
+{
+    private readonly PwmChannel _pwm;
+
+    public StatusLED(int gpioPin = 18, int frequency = 1000)
+    {
+        _pwm = new SoftwarePwmChannel(gpioPin, frequency, dutyCycle: 0.0, usePrecisionTimer: true);
+        _pwm.Start();
+    }
+
+    public void SetBrightness(double level)
+    {
+        // level skal være mellem 0.0 (slukket) og 1.0 (fuldt lys)
+        _pwm.DutyCycle = Math.Clamp(level, 0.0, 1.0);
+    }
+
+    public void TurnOn() => SetBrightness(1.0);
+    public void TurnOff() => SetBrightness(0.0);
+
+    public void Dispose()
+    {
+        _pwm?.Dispose();
+    }
+}
 

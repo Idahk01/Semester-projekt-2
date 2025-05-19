9c8db7e802992c39630ecdd38680aea80a234c04
frejarandel <freja.randel@gmail.com>
2025-05-19 15:13:04 +0200

    Update Program.cs

--- Version5/Program.cs
+++ Version5/Program.cs
@@ -2,7 +2,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        var system = new TændSlukSystem();
+        // Sæt din Home Assistant-url og token her:
+        string baseUrl     = "http://172.20.10.6:8123";
+        string bearerToken = "DIN_LONG_LIVED_ACCESS_TOKEN";
+
+        var system = new TændSlukSystem(baseUrl, bearerToken);
         system.Start();
     }
 }

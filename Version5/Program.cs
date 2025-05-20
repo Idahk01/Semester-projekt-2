50c0f3f5073455f8d085b2f10b641fcb9563e009
jonasbebew <jonas.bebe447@gmail.com>
2025-05-20 10:38:05 +0200

    Update Program.cs

--- Version5/Program.cs
+++ Version5/Program.cs
@@ -4,7 +4,7 @@ class Program
     {
         // Sæt din Home Assistant-url og token her:
         string baseUrl     = "http://172.20.10.6:8123";
-        string bearerToken = "DIN_LONG_LIVED_ACCESS_TOKEN";
+        string bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmMDFjOGJjMzBhNGE0NDAzYWJiNWU5NjdjYzdiZjU1MiIsImlhdCI6MTc0Mzk2OTY5NiwiZXhwIjoyMDU5MzI5Njk2fQ.perXXUfe-44-SzCOS1t1h-4BZ0AxvOiRTbI-16yxVvU";
 
         var system = new TændSlukSystem(baseUrl, bearerToken);
         system.Start();

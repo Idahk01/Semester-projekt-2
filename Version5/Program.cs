89b714afc06beb8edd07dbe847202bb4ea3f9ac5
JeppeVit <132815647+JeppeVit@users.noreply.github.com>
2025-05-20 17:33:50 +0200

    Update Program.cs

--- Version5/Program.cs
+++ Version5/Program.cs
@@ -2,7 +2,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Sæt din Home Assistant-url og token her:
+        // Homeassistant adresse + token
         string baseUrl     = "http://172.20.10.6:8123";
         string bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmMDFjOGJjMzBhNGE0NDAzYWJiNWU5NjdjYzdiZjU1MiIsImlhdCI6MTc0Mzk2OTY5NiwiZXhwIjoyMDU5MzI5Njk2fQ.perXXUfe-44-SzCOS1t1h-4BZ0AxvOiRTbI-16yxVvU";
 
